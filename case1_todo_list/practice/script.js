70050a7209c050c327b00649a11eb014d29cc861
cloudedpanther <rickeykoh@ajou.ac.kr>
2023-02-25 23:03:49 +0900

    add basic todo list

--- case1_todo_list/practice/script.js
+++ case1_todo_list/practice/script.js
@@ -5,8 +5,14 @@
     return document.querySelector(target)
   }
 
+  const API_URL = 'http://localhost:3000/todos'
+  const $todos = get('.todos')
+  const $todoForm = get('.todo_form')
+  const $todoInput = get('.todo_input')
+
   const createTodoElement = (item) => {
-    const { id, content } = item
+    const { id, content, completed } = item
+    const isChecked = completed ? 'checked' : ''
     const $todoItem = document.createElement('div')
     $todoItem.classList.add('item')
     $todoItem.dataset.id = id
@@ -15,6 +21,7 @@
               <input
                 type="checkbox"
                 class='todo_checkbox' 
+                ${isChecked}
               />
               <label>${content}</label>
               <input type="text" value="${content}" />
@@ -39,6 +46,132 @@
     return $todoItem
   }
 
-  const init = () => {}
+  const renderAllTodos = (todos) => {
+    $todos.innerHTML = ''
+    todos.forEach((item) => {
+      const todoElement = createTodoElement(item)
+      $todos.appendChild(todoElement)
+    })
+  }
+
+  const getTodos = () => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((todos) => renderAllTodos(todos))
+      .catch((error) => console.log(error))
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault()
+
+    const todo = {
+      content: $todoInput.value,
+      completed: false,
+    }
+
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(todo),
+    })
+      .then(getTodos)
+      .then(() => {
+        $todoInput.value = ''
+        $todoInput.focus()
+      })
+      .catch((error) => console.log(error))
+  }
+
+  const toggleTodo = (e) => {
+    if (e.target.className !== 'todo_checkbox') return
+
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+    const completed = e.target.checked
+
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ completed }),
+    })
+      .then(getTodos)
+      .catch((error) => console.log(error))
+  }
+
+  const changeEditMode = (e) => {
+    const $item = e.target.closest('.item')
+    const $label = $item.querySelector('label')
+    const $editInput = $item.querySelector('input[type="text"]')
+    const $contentButtons = $item.querySelector('.content_buttons')
+    const $editButtons = $item.querySelector('.edit_buttons')
+
+    if (e.target.className === 'todo_edit_button') {
+      $label.style.display = 'none'
+      $editInput.style.display = 'block'
+      $contentButtons.style.display = 'none'
+      $editButtons.style.display = 'flex'
+
+      $editInput.focus()
+      $editInput.value = ''
+      $editInput.value = $label.innerText
+    }
+
+    if (e.target.className === 'todo_edit_cancel_button') {
+      $label.style.display = 'block'
+      $editInput.style.display = 'none'
+      $contentButtons.style.display = 'flex'
+      $editButtons.style.display = 'none'
+
+      $editInput.value = ''
+      $editInput.value = $label.innerText
+    }
+  }
+
+  const editTodo = (e) => {
+    if (e.target.className !== 'todo_edit_confirm_button') return
+
+    const $item = e.target.closest('.item')
+    const $editInput = $item.querySelector('input[type="text"]')
+    const id = $item.dataset.id
+    const content = $editInput.value
+
+    fetch(`${API_URL}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ content }),
+    })
+      .then(getTodos)
+      .catch((error) => console.log(error))
+  }
+
+  const removeTodo = (e) => {
+    if (e.target.className !== 'todo_remove_button') return
+
+    const $item = e.target.closest('.item')
+    const id = $item.dataset.id
+
+    fetch(`${API_URL}/${id}`, {
+      method: 'DELETE',
+    })
+      .then(getTodos)
+      .catch((error) => console.log(error))
+  }
+
+  const init = () => {
+    window.addEventListener('DOMContentLoaded', () => {
+      getTodos()
+    })
+    $todoForm.addEventListener('submit', addTodo)
+    $todos.addEventListener('click', toggleTodo)
+    $todos.addEventListener('click', changeEditMode)
+    $todos.addEventListener('click', editTodo)
+    $todos.addEventListener('click', removeTodo)
+  }
   init()
 })()
